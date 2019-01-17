bcee5e8eeae4472a8b67fbdd6ced04ea28d22d57
Dipanjan Bhowmik <onlydevelop@gmail.com>
2019-01-17 13:50:22 +0530

    Minor refactor

--- translate.py
+++ translate.py
@@ -7,15 +7,19 @@ import pygame
 REGION = 'us-east-1'
 
 # Constants
-SOURCE_FILE = "source.txt"
 SOURCE_LANGUAGE_CODE = "en"
 TARGET_LANGUAGE_CODE = "de"
+
+# Filenames
+SOURCE_FILE = "source.txt"
+TRANSLATED_FILE = "translated.txt"
 AUDIO_FILENAME = "speech.mp3"
 
 def main():
     # The source text was generated from fairy tale generator:
     # https://www.plot-generator.org.uk/fairytale/
     translated_text = translate(SOURCE_FILE, SOURCE_LANGUAGE_CODE, TARGET_LANGUAGE_CODE)
+    print(translated_text)
     get_audio(translated_text, AUDIO_FILENAME)
     play_audio(AUDIO_FILENAME)
     
@@ -23,8 +27,7 @@ def main():
 def translate(source_file, souce_language_code, target_language_code):
     print(f"Translating text from {souce_language_code} to {target_language_code}...")
     translate = boto3.client(service_name='translate', region_name=REGION, use_ssl=True)
-    f = open(source_file, "r")
-    text = f.read()
+    text = read_file(source_file)
 
     result = translate.translate_text(Text=text, 
                 SourceLanguageCode=souce_language_code, 
@@ -51,7 +54,14 @@ def play_audio(audio_filename):
     pygame.mixer.music.load(audio_filename)
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy(): 
+  
         pygame.time.Clock().tick(10)
+
+
+def read_file(filename):
+    f = open(filename, "r")
+    content = f.read()
+    return content
 
 
 if __name__ == '__main__':
